b0b9d1691f0ef682cceda99c41a96e0c3f664926
Sean Jiang <seanjiangsh@gmail.com>
2024-10-10 19:48:26 +0800

    feat: added arc length and angles utilities

--- utils/circles.py
+++ utils/circles.py
@@ -1,7 +1,9 @@
+import sympy
+import math
 from sympy import symbols, Rational, simplify, sqrt
 
 
-def find_circle_equation(A, B, D, E, F):
+def find_circle_equation(A: float, B: float, D: float, E: float, F: float):
     """
     Finds the equation of a circle given the coefficients of the general form 
     Ax^2 + By^2 + Dx + Ey + F = 0. Assumes A == B for a circle.
@@ -53,3 +55,58 @@ def find_circle_equation(A, B, D, E, F):
     # Return the equation, center, and radius
     center = (h, k)
     return circle_equation, center, radius
+
+
+def get_arc_length(radius: float, angle: float, symbolic=False):
+    """
+    Calculates the arc length of a circle given the radius and angle in degrees.
+    
+    Parameters:
+    radius (float): The radius of the circle.
+    angle (float): The angle in degrees.
+    symbolic (bool): If True, returns the symbolic expression.
+    
+    Returns:
+    float: The arc length of the circle.
+    
+    Formula:
+    arc_length = radius * angle * (pi / 180)
+    or
+    arc_length = (angle/360) * (2 * pi * radius)
+
+    Example:
+    get_arc_length(5, 90)
+    """
+
+    # Convert the angle to radians
+    pi = sympy.pi if symbolic else math.pi
+    angle_radians = angle * (pi / 180)
+
+    # Calculate the arc length
+    arc_length = radius * angle_radians
+    return arc_length
+
+
+def get_angle_by_arc_length(radius: float, arc_length: float, symbolic=False):
+    """
+    Calculates the angle of a circle given the radius and arc length.
+    
+    Parameters:
+    radius (float): The radius of the circle.
+    arc_length (float): The arc length.
+    symbolic (bool): If True, returns the symbolic expression.
+    
+    Returns:
+    float: The angle of the circle in degrees.
+    
+    Formula:
+    angle = (arc_length / radius) * (180 / pi)
+
+    Example:
+    get_angle_by_arc_length(5, 5)
+    """
+
+    # Convert the angle to degrees
+    pi = sympy.pi if symbolic else math.pi
+    angle = (arc_length / radius) * (180 / pi)
+    return angle
